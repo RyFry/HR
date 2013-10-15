cb6760c09c69b56066d50b03aa8f3f51eb884798
RyFry <ryan.lorey@utexas.edu>
2013-10-14 20:00:47 -0500

    removed sisyphus folder

--- HR/src/com/sisyphus/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package com.sisyphus;
-
-public class GlobalConstants {
-	public static final int NUM_TAGS=0;
-	public static final long STARTING_POP = 1000,
-							 STARTING_RES = 1000,
-							 RATE = 1,
-							 MAX = 10000;
-}
